6decf52ea5502df42b6384b734e19878c5b4a153
fardeen077 <fardeen36143@gmail.com>
2025-09-27 12:56:06 +0530

    Fix ThemeContext background images and toggle

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,8 +1,12 @@
 import { createContext, useContext, useState } from "react";
+import BgDark from "../../images/bg-desktop-dark.jpg"
+import BgLight from "../../images/bg-desktop-light.jpg";
+
 
 const ThemeContext = createContext({
     theme: "light",
-    handleToggle: () => { }
+    handleToggle: () => { },
+    bgImage: BgLight,
 });
 
 export const useTheme = () => {
@@ -11,10 +15,12 @@ export const useTheme = () => {
 
 export const ThemeProvider = ({ children }) => {
     const [theme, setTheme] = useState("light");
+
     const handleToggle = () => {
         setTheme((prev) => (prev === "light" ? "dark" : "light"));
     };
-    const bgImage = theme === "light" ? "../images/bg-desktop-dark.jpg " : "../images/bg-desktop-light.jpg";
+    const bgImage = theme === "light" ? BgDark : BgLight;
+
     return (
         <ThemeContext.Provider value={{ theme, handleToggle, bgImage }}>
             <div className={`min-h-screen transition-all duration-300 ${theme === "light" ? "bg-gray-100 text-black" : "bg-gray-900 text-white"}`}>
